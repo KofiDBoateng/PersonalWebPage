c9b17b861dab39489828b7c7fe291c7b42e3e259
Swank123 <kdboat2@gmail.com>
2021-11-28 15:17:05 -0600

    deleted certs and added data science section to works. I will change the styles and continue working on web development section.

--- app.js
+++ app.js
@@ -3,37 +3,28 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
 
-
 app.use(express.static("Public"));
 app.set("view engine", "ejs");
 
-
-
 /////////////////////////////////////////////////Home///////////////////////////////////////////////////
-app.get("/", function(req, res){
-    res.render("home")
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
 /////////////////////////////////////////////////Contact///////////////////////////////////////////////
-app.get("/contact", function(req, res){
-    res.render("contact")
-})
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
 
 /////////////////////////////////////////////////Work///////////////////////////////////////////////
-app.get("/works", function(req, res){
-    res.render("certs")
-})
+app.get("/works", function (req, res) {
+  res.render("works");
+});
 
-/////////////////////////////////////////////////Certs///////////////////////////////////////////////
-app.get("/certs", function(req, res){
-    res.render("certs")
-})
-
-
-app.listen(3000, function(err){
-    if(!err){
-        console.log("Server successfully running")
-    }else{
-        console.log(err);
-    }
-})
+app.listen(3000, function (err) {
+  if (!err) {
+    console.log("Server successfully running");
+  } else {
+    console.log(err);
+  }
+});
